4f9f1361f98703c296853d56782b29fbf98633da
xuantruong2004 <truongxuantp3@gmail.com>
2022-11-26 16:39:55 +0700

    update API render server

--- src/api/ApiRequest.js
+++ src/api/ApiRequest.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://truongxuan-face-server.herokuapp.com/",
+  // baseURL: "https://truongxuan-face-server.herokuapp.com/",
+  baseURL: "https://truongxuan-fb-server.onrender.com/",
 });
 export default API;
